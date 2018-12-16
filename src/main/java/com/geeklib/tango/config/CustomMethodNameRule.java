fb43470fa22644591f98c8e2c450fa539d1ef8f6
geek1st <geek1st@live.com>
2018-12-17 00:40:28 +0800

    修复XMLMAPPER元素ID与MAPPER方法名保持一直

--- src/main/java/com/geeklib/tango/config/CustomMethodNameRule.java
+++ src/main/java/com/geeklib/tango/config/CustomMethodNameRule.java
@@ -7,6 +7,7 @@ import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Interface;
 import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.Element;
 import org.mybatis.generator.api.dom.xml.XmlElement;
@@ -26,8 +27,9 @@ public class CustomMethodNameRule extends PluginAdapter  {
 		String name = introspectedTable.getFullyQualifiedTableNameAtRuntime();
 		name = StringUtils.substringAfter(name, "_");
 		name = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, name);
-		name = "update" + name + "byId";
+		name = "update" + name + "ById";
 		method.setName(name);
+		introspectedTable.setUpdateByPrimaryKeyStatementId(name);
 		return super.clientUpdateByPrimaryKeyWithoutBLOBsMethodGenerated(method, interfaze, introspectedTable);
 	}
 	
@@ -40,7 +42,7 @@ public class CustomMethodNameRule extends PluginAdapter  {
 		name = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, name);
 		name = "list" + name;
 		method.setName(name);
-		
+		introspectedTable.setSelectAllStatementId(name);
 		return super.clientSelectAllMethodGenerated(method, interfaze, introspectedTable);
 	}
 	
@@ -53,7 +55,7 @@ public class CustomMethodNameRule extends PluginAdapter  {
 		name = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, name);
 		name = "save" + name;
 		method.setName(name);
-		
+		introspectedTable.setInsertStatementId(name);
 		return super.clientInsertMethodGenerated(method, interfaze, introspectedTable);
 	}
 	
@@ -65,7 +67,7 @@ public class CustomMethodNameRule extends PluginAdapter  {
 		name = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, name);
 		name = "get" + name + "ById";
 		method.setName(name);
-		
+		introspectedTable.setSelectByPrimaryKeyStatementId(name);
 		return super.clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable);
 	}
 	
@@ -77,28 +79,8 @@ public class CustomMethodNameRule extends PluginAdapter  {
 		name = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, name);
 		name = "remove" + name + "ById";
 		method.setName(name);
-		
+		introspectedTable.setDeleteByPrimaryKeyStatementId(name);
 		return super.clientDeleteByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable);
 	}
 	
-	
-	@Override
-	public boolean sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-		
-		String name = introspectedTable.getFullyQualifiedTableNameAtRuntime();
-		name = StringUtils.substringAfter(name, "_");
-		name = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, name);
-		name = "update" + name + "ById";
-		
-//		Attribute attribute = new Attribute("id", name);
-//		element.addAttribute(attribute);
-		
-		return super.sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(element, introspectedTable);
-	}
-	
-	@Override
-	public boolean sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-		
-		return super.sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(element, introspectedTable);
-	}
 }
